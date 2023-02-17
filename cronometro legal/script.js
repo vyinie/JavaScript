d259ee1a468b755e4e72b27fa43696b665dfc501
vyinie <marcusvyinicius@gmail.com>
2023-02-17 17:04:27 -0300

    cronometro crescentre

--- cronometro legal/script.js
+++ cronometro legal/script.js
@@ -1,15 +1,17 @@
 let seg = 0;
 let min = 0;
 let hor = 0;
-let ligado = false;
+let contando_tempo = false;
 
 // inicia a função que roda tudo
 function iniciar() {
-  if (ligado === false) {
+  if (contando_tempo === false) {
     contar();
-    tempo = setInterval(contar, 1000);
-    ligado = true;
-  }else{!iniciar}
+    tempo = setInterval(contar, 100);
+    contando_tempo = true;
+  } else {
+    !iniciar;
+  }
 }
 
 // função que roda tudo
@@ -44,16 +46,59 @@ function tela(num) {
 // pausa
 function pausar() {
   clearInterval(tempo);
-  ligado = false;
+  contando_tempo = false;
 }
 
 // zera o relogio
 function parar() {
   clearInterval(tempo);
-  alert(tela(hor) + ":" + tela(min) + ":" + tela(seg));
+  contando_tempo = false;
+
+  // historico temporal oOoOoOoOOoOoOokk
+  if (hist_ligado === true) {
+    let li = document.createElement("li");
+    let dados = tela(hor) + ":" + tela(min) + ":" + tela(seg);
+    let lista = document.getElementById("ul");
+    li.textContent = dados;
+    lista.appendChild(li);
+  }
+  // permissão do pop-up
+  if (popup_ligado === true) {
+    alert(tela(hor) + ":" + tela(min) + ":" + tela(seg));
+  }
+
+  // zera a contagem
   seg = 0;
   min = 0;
   hor = 0;
   document.getElementById("tela").innerHTML = "00:00:00";
-  ligado = false;
+
+  // carrega o historico
+  window.addEventListener("load", registro);
+}
+
+// desativa o pop-up
+let popup_ligado = true;
+
+function liga_popup() {
+  if (popup_ligado === true) {
+    popup_ligado = false;
+    document.getElementById('pop_up').innerHTML = "ativar pop-up"
+  } else {
+    popup_ligado = true;
+    document.getElementById('pop_up').innerHTML = "desativar pop-up"
+  }
+}
+
+// desativa o historico
+let hist_ligado = true;
+
+function liga_hist() {
+  if (hist_ligado === true) {
+    hist_ligado = false;
+    document.getElementById('hist').innerHTML = "ativar hitórico"
+  } else {
+    hist_ligado = true;
+    document.getElementById('hist').innerHTML = "desativar hitórico"
+  }
 }
