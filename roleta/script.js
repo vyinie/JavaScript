035e5b0d2562a7e435f46857c277cea03bb8a474
vyinie <marcusvyinicius@gmail.com>
2023-03-16 11:15:49 -0300

    roleta 0.1

--- roleta/script.js
+++ roleta/script.js
@@ -1,52 +1,60 @@
 // preventDefalt
 const form = document.querySelector("form");
-form.addEventListener("click" | "enter", () => {
+form.addEventListener("submit", () => {
   event.preventDefault();
 });
 
-// variaveis
+// ==========================variaveis==========================
 const $inpTxt = document.querySelector("#inpTxt");
 const $inpBtn = document.querySelector("#inpBtn");
 const $ul = document.querySelector("#ul");
 const $btnGirar = document.querySelector("#btnGirar");
 const $inpDel = document.querySelector("#inpDel");
-// ===============================================
+// ======================outras variaveis=======================
 const opcoes = [];
-// ===============================================
+var id = 0;
 
-// add opcoes a rolerta
+// ====================add opcoes a rolerta=====================
 $inpBtn.addEventListener("click", () => {
   if ($inpTxt.value != "") {
     $ul.insertAdjacentHTML(
       "beforeend",
       `<li onclick="delLi()"
-       id="${opcoes.length}">${$inpTxt.value}</li>`
+       id="${id}">${$inpTxt.value}</li>`
     );
-    opcoes.push({
-      id: opcoes.length,
-      text: $inpTxt.value,
-    });
+    opcoes.push($inpTxt.value);
     $inpTxt.focus();
     $inpTxt.value = "";
+    id++;
   } else {
     $inpTxt.focus();
   }
 });
 
-// limpa a lista toda
+// =====================limpa a lista toda======================
 $inpDel.addEventListener("click", () => {
   opcoes.splice(0, opcoes.length);
+  id = 0;
   $ul.innerHTML = "";
 });
 
+// =================deleta uma opcao especifica=================
+function mudaLugar(daqui, praca) {
+  var item = opcoes[daqui];
+  opcoes.splice(daqui, 1);
+  opcoes.splice(praca, 0, item);
+}
+
 function delLi() {
   var target = event.target.id;
-  var removeId = document.getElementById(`id${target}`);
   $ul.removeChild(document.getElementById(target));
-  if(opcoes.some((e) => target)){
-}}
 
-// gira essa bagaça
+  const index = opcoes.indexOf(event.target.textContent)
+  mudaLugar(index, 0);
+  opcoes.shift()
+}
+
+// ======================gira essa bagaça=======================
 $btnGirar.addEventListener("click", () => {
   const random = Math.floor(Math.random() * opcoes.length);
   console.log(opcoes[random]);
