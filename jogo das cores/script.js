057f129ec002a87c6462e0782e723b7c50a27d28
Marcus Vinicius <110700931+vyinie@users.noreply.github.com>
2023-03-30 17:44:11 -0300

    jogo das cores 1.2

--- jogo das cores/script.js
+++ jogo das cores/script.js
@@ -1,9 +1,12 @@
 // ==========================ids HTML===========================
 const $divCor = document.querySelector("#divCor");
 const $historico = document.querySelector("#historico");
-// Linha_21, Linha_33
+const $opcoes = document.querySelector("#opcoes");
+const $divVidas = document.querySelector("#divVidas");
+const $popupFundo = document.querySelector("#popupFundo");
+const $btnReset = document.querySelector("#btnReset");
 
-// ============================gears============================
+// ==========================functions==========================
 
 // randomiza as cores
 function random() {
@@ -14,35 +17,84 @@ function random() {
   var corEscolhida = `${r}, ${g}, ${b}`;
   return corEscolhida;
 }
-
-// escreve as opçcoes
-function textOpc() {
+// =============================================================
+function opcoesErradas() {
   for (var opcId = 0; opcId <= 5; opcId++) {
     document.querySelector(`#opc${opcId}`).innerHTML = random();
+    document.querySelector(`#opc${opcId}`).style.backgroundColor = "";
+    document.querySelector(`#opc${opcId}`).style.color = "#000";
   }
 }
 
-// ========================começa o jogo========================
+// =============================================================
+let idVidas = 3;
+function animOpcaoErrda(e) {
+  e.target.style.backgroundColor = "crimson";
+  e.target.style.color = "#fff";
+  setTimeout(function () {
+    e.target.style.backgroundColor = "transparent";
+    e.target.style.color = "transparent";
+    e.target.style.pointerEvents = "none";
+  }, 500);
 
+  document.querySelector(`#vida${idVidas}`).style.display = "none";
+  idVidas--;
+
+  if (idVidas == 0) {
+    $popupFundo.style.display = "block";
+  }
+}
+
+// =============================================================
 function respostaCerta() {
   const corFixa = random();
   $divCor.style.background = `rgb(${corFixa})`;
-  textOpc();
+  opcoesErradas();
 
   var opcCerta = Math.floor(Math.random() * 5);
   document.querySelector(`#opc${opcCerta}`).innerHTML = corFixa;
 }
+
+// =============================================================
+function animacaoAcerto(e) {
+  e.target.style.background = $divCor.style.background;
+  setTimeout(() => {
+    hist();
+    respostaCerta();
+    e.target.style.backgroundColor = "";
+  }, 500);
+  for (i = 0; i <= 5; i++) {
+    document.querySelector(`#opc${i}`).style.pointerEvents = "";
+  }
+}
+
+// ========================começa o jogo========================
 respostaCerta();
 
 // analiza as opçoes
-document.addEventListener("click", (e) => {
-  if ($divCor.style.background === `rgb(${e.target.textContent})`) {
-    e.target.style.background = $divCor.style.background;
-    setTimeout(() => {
-      hist();
-      respostaCerta();
-      e.target.style.backgroundColor = "";
-    }, 1000);
+$opcoes.addEventListener("click", (e) => {
+  if (e.target.className === "btnResp") {
+    if ($divCor.style.background === `rgb(${e.target.textContent})`) {
+      animacaoAcerto(e);
+      if (idVidas != 3) {
+        idVidas++;
+        document.querySelector(`#vida${idVidas}`).style.display = "";
+      }
+    } else {
+      animOpcaoErrda(e);
+    }
+  }
+});
+
+$btnReset.addEventListener("click", () => {
+  respostaCerta();
+  $popupFundo.style.display = "none";
+  idVidas = 3;
+  for (i = 1; i <= 3; i++) {
+    document.querySelector(`#vida${i}`).style.display = "";
+  }
+  for (i = 0; i <= 5; i++) {
+    document.querySelector(`#opc${i}`).style.pointerEvents = "";
   }
 });
 
@@ -59,12 +111,11 @@ function hist() {
   span.innerHTML = spanCont;
 }
 
-
-// copia 
+// copia
 $historico.addEventListener("click", (e) => {
   if (e.target.className == "corHist") {
     let copy = e.target.style.backgroundColor.slice(4, -1);
-    navigator.clipboard.writeText(copy)
-    alert('rgb copiado '+ copy )
+    navigator.clipboard.writeText(copy);
+    alert("rgb copiado " + copy);
   }
 });
